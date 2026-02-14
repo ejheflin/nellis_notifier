368489ffbf8cb89771c80f4776794c741a83d00e
Eric <ejheflin@gmail.com>
2026-02-14 06:39:31 -0600

    Update nellis_monitor_playwright.py
    updated urls

--- nellis_monitor_playwright.py
+++ nellis_monitor_playwright.py
@@ -42,7 +42,7 @@ URLS: List[str] = [
     "https://nellisauction.com/search?query=mens+13+Allen+Edmonds",
     "https://nellisauction.com/search?query=national+tree+company+flocked+9",
     "https://nellisauction.com/search?Location+Name=Katy&query=king+mattress&Suggested+Retail=500&Star+Rating=4.0&Star+Rating=5.0",
-    
+    "https://nellisauction.com/search?Location+Name=Katy&query=basketball+hoop&Taxonomy+Level+1=Outdoors+%26+Sports&Suggested+Retail=200"
 ]
 
 # Optional friendly names for RSS titles (otherwise URL is used)
